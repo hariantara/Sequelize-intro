616fd5c032bd75ecbe22fed75095c1594aa9dc68
hariantara <hariantara.iputu@gmail.com>
2017-07-13 22:04:48 +0700

    sequelize v1

--- routers/subject.js
+++ routers/subject.js
@@ -2,12 +2,35 @@ const express = require('express');
 var router = express.Router();
 const database = require('../models/');
 
+//data subject
 router.get('/', function(req, res){
   database.Subject.findAll()
   .then((results) =>{
     res.render('subjects', {dataSubjects:results})
   });
 });
+//show form addColumn
+router.get('/add', function(req, res){
+    res.render('sform')
+});
+//add subject data
+router.post('/add', function(req, res){
+  database.Subject.create({
+    subject_name: req.body.subjectname,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  })
+  .then(() =>{
+      res.redirect('/subject')
+  })
+})
+//edit form
+router.get('/edit/:id', function(req, res){
+  database.Subject.findById(req.params.id)
+  .then((result) =>{
+    res.render('editsubject', {id: result})
+  })
+})
 
 
 module.exports = router;
